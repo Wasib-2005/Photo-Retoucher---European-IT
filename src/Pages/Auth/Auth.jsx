ad2ece74ae8bd0e84d1a73dd81706249910a7e7a
Wasib <wasibulislam8984@gmail.com>
2026-07-29 23:29:41 +0600

    - Add Register done

--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -2,20 +2,21 @@ import { useParams } from "react-router";
 import Right_crave from "../../assets/Right_crave.svg";
 import Left_crave from "../../assets/Left_crave.svg";
 import Login from "../../Components/Navbar/Auth/Login";
+import Register from "../../Components/Navbar/Auth/Register";
 
 const Auth = () => {
   const { page } = useParams();
 
   return (
-    <div className="relative w-screen h-screen overflow-hidden bg-white">
-      {/* 1st Image: Top Left */}
+    <div className="relative w-screen min-h-screen overflow-x-hidden bg-white pt-10">
+      {/* 1st Image: Top Right */}
       <img
         className="absolute top-0 right-0 max-w-[15%] pointer-events-none select-none"
         src={Right_crave}
         alt=""
       />
 
-      {/* 2nd Image: Bottom Right */}
+      {/* 2nd Image: Bottom Left */}
       <img
         className="absolute bottom-0 left-0 max-w-[15%] pointer-events-none select-none"
         src={Left_crave}
@@ -23,14 +24,8 @@ const Auth = () => {
       />
 
       {/* Main Content Area */}
-      <div className="relative z-10 flex items-center justify-center h-full">
-        {page === "login" ? (
-          <>
-            <Login />
-          </>
-        ) : (
-          <></>
-        )}
+      <div className="relative z-10 flex justify-center">
+        {page === "login" ? <Login /> : <Register />}
       </div>
     </div>
   );
