e39ccab993bd556fbc332c62800a47d3f86e060a
Wasib <wasibulislam8984@gmail.com>
2026-08-05 23:56:36 +0600

    Add new SVG assets for ShareDetailsFinishOrder and WeEditImages one part left in How it works page

---
+++ src/Components/HowItWorkComponent/ShareDetailsFinishOrderComponent.jsx
@@ -0,0 +1,53 @@
+import CheckboxCircleFill from "../../assets/CheckboxCircleFill.svg";
+import ShareDetailsFinishOrder from "../../assets/ShareDetailsFinishOrder.svg";
+
+const ShareDetailsFinishOrderComponent = () => {
+  return (
+    <div className=" rounded-2xl flex flex-col lg:flex-row justify-between items-center ">
+      <img className="h-full" src={ShareDetailsFinishOrder} alt="" />
+
+      <div className="flex flex-col gap-2 md:gap-5 px-6 md:px-12 lg:px-18 py-6 md:py-12 ">
+        <div>
+          <h2 className="text-[14px] md:text-[16px] lg:text-[18px] text-[#0041FF] font-medium">
+            Step 3
+          </h2>
+
+          <h1 className="text-[18px] md:text-[24px] lg:text-[28px] text-[#1B263B] font-medium max-w-100">
+            Share any details and finish up your order.
+          </h1>
+        </div>
+
+        <p className="max-w-140">
+          Need to share something with your designer? You can add instructions
+          and attach supporting files—or skip this step if you don’t have
+          anything to share.
+          <br />
+          <br />
+          <p>
+            You can also make final adjustments to timing, number of images,
+            file format, and copies—and add free straightening, cropping, or
+            resizing to your order. When you’re all set, finish up your order,
+            and we’ll get to work.
+          </p>
+        </p>
+
+        <div className="flex flex-col gap-1">
+          <div className="flex gap-2 items-center">
+            <img src={CheckboxCircleFill} alt="" />
+            <p>Bulk Upload</p>
+          </div>
+          <div className="flex gap-2 items-center">
+            <img src={CheckboxCircleFill} alt="" />
+            <p>Secure & Safe</p>
+          </div>
+          <div className="flex gap-2 items-center">
+            <img src={CheckboxCircleFill} alt="" />
+            <p>All file formats supports</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShareDetailsFinishOrderComponent;
--- src/Pages/HowItWorkPage/HowItWorkPage.jsx
+++ src/Pages/HowItWorkPage/HowItWorkPage.jsx
@@ -1,5 +1,13 @@
+import BusinessReview from "../../Components/Common/BusinessReview/BusinessReview";
+import CalculatePrice from "../../Components/Common/CalculatePrice/CalculatePrice";
+import CommonFeacher2 from "../../Components/Common/CommonFeacher2";
+import FAQComponent from "../../Components/Common/FAQComponent/FAQComponent";
+import ChooseTurnaroundTimeComponent from "../../Components/HowItWorkComponent/ChooseTurnaroundTimeComponent";
+import DownloadProfessionallyEditedImagesComponent from "../../Components/HowItWorkComponent/DownloadProfessionallyEditedImagesComponent";
 import HowItWorkTitle from "../../Components/HowItWorkComponent/HowItWorkTitle";
 import SelectServiceUpload from "../../Components/HowItWorkComponent/SelectServiceUpload/SelectServiceUpload";
+import ShareDetailsFinishOrderComponent from "../../Components/HowItWorkComponent/ShareDetailsFinishOrderComponent";
+import WeEditImagesComponent from "../../Components/HowItWorkComponent/WeEditImagesComponent";
 import NavbarSpece from "../../Components/Navbar/NavbarSpece";
 
 const HowItWorkPage = () => {
@@ -11,6 +19,40 @@ const HowItWorkPage = () => {
 
       <div className="mx-auto px-4 md:px-12 lg:px-20 py-5 md:py-10 lg:py-20">
         <SelectServiceUpload />
+      </div>
+
+      <div className="mx-auto px-4 md:px-12 lg:px-20 py-5 md:py-10 lg:py-20">
+        <ChooseTurnaroundTimeComponent />
+      </div>
+
+      <div className="mx-auto px-4 md:px-12 lg:px-20 py-5 md:py-10 lg:py-20">
+        <ShareDetailsFinishOrderComponent />
+      </div>
+
+      <div className="mx-auto px-4 md:px-12 lg:px-20 py-5 md:py-10 lg:py-20">
+        <WeEditImagesComponent />
+      </div>
+
+      <div className="mx-auto px-4 md:px-12 lg:px-20 py-5 md:py-10 lg:py-20">
+        <DownloadProfessionallyEditedImagesComponent />
+      </div>
+
+      <div className="mx-auto px-4 md:px-12 lg:px-20 py-5 md:py-10 lg:py-20">
+        <CommonFeacher2 />
+      </div>
+
+      {/* TODO add the figma section */}
+
+      <div className="mx-auto px-4 md:px-12 lg:px-20 py-5 md:py-10 lg:py-20">
+        <CalculatePrice />
+      </div>
+
+      <div className="mx-auto px-4 md:px-12 lg:px-20 py-5 md:py-10 lg:py-20">
+        <BusinessReview />
+      </div>
+
+      <div className="mx-auto px-4 md:px-12 lg:px-20 py-5 md:py-10 lg:py-20">
+        <FAQComponent />
       </div>
     </div>
   );
